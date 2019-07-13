5a13df0db8bfd60a70f45b74af90bae97a581e30
Martin Grossert <martin.grossert@gmail.com>
2019-07-13 12:47:19 +0200

    ignore chart errors

--- src/ts/app/frontend/StarView.ts
+++ src/ts/app/frontend/StarView.ts
@@ -1,4 +1,6 @@
+// @ts-ignore
 import * as am4core from "@amcharts/amcharts4/core";
+// @ts-ignore
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import { AbstractView } from "../abstract/AbstractView";
